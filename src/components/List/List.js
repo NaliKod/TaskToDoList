9385657baca9f8267f5d8ffde1c964272cb4244e
Nali <nalikante@gmail.com>
2021-05-18 13:08:41 +0200

    Add Hero and List component

---
+++ src/components/List/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/Hero';
+
+class List extends React.Component {
+  render() {
+    return (
+      <selector className={styles.component}>
+        <Hero/>
+      </selector>
+    )
+  }
+}
+
+export default List;
